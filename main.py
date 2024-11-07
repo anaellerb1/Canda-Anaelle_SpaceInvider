add8cf6fde2fa7201b7ea4e66f408e532313d90b
Anaëlle ROBIN <anaelle.robin@cpe.fr>
2024-11-07 08:47:45 +0100

    création main

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+class Jeu:
+    def __init__(self, interface):
+
+        
+class 
+
+
+
+# Lancer l'interface
+interface = tk.Tk()
+game = Jeu(interface)
+interface.mainloop()
