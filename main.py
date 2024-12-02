fbcfd8212e2db020e485cc5789a2d865ff7a15f0
SanjayCANDA <candasanjay1@gmail.com>
2024-12-02 16:07:41 +0100

    oui

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-"""
-Anaëlle ROBIN  & Sanjay CANDA 3ETI
-07/11/2024
-
-Fichier de la classe pour créer le jeu
-
-Status: 
-       EN COURS
-"""
 import tkinter as tk
+from tkinter import Menu
 from Alien import Alien
 from Player import Player
 from Torpille import Torpille
@@ -19,12 +11,13 @@ class Jeu:
         self.score = 0
         self.LARGEUR = 200
         self.HAUTEUR = 400
+        self.jeu_en_pause = False  # État du jeu (pause ou non)
 
         self.interface = interface
         self.interface.title("Space Invader - Sanjay x Anaëlle")
 
         # Fenêtre
-        self.HAUTEUR = self.interface.winfo_screenheight() - 100  # Ajuster la hauteur pour laisser de la place en bas
+        self.HAUTEUR = self.interface.winfo_screenheight() - 100
         self.LARGEUR = self.interface.winfo_screenwidth() // 3
         self.center_window(self.LARGEUR, self.HAUTEUR)
         
@@ -47,7 +40,7 @@ class Jeu:
         self.score_label.pack()
 
         # Start button
-        start_button = tk.Button(self.frame, text="Démarrer", command=self.start_game)
+        start_button = tk.Button(self.frame, text="Jouer", command=self.start_game)
         start_button.pack(side=tk.LEFT, padx=10)
 
         # Menu 'option'
@@ -58,7 +51,8 @@ class Jeu:
         menubar.add_cascade(label="Options", menu=game_menu)
         game_menu.add_command(label="Quitter", command=self.interface.quit)
 
-        
+        # Lier Échap au menu pause
+        self.interface.bind("<Escape>", self.afficher_menu_pause)
 
     def center_window(self, width, height):
         """Centre la fenêtre sur l'écran."""
@@ -74,11 +68,12 @@ class Jeu:
         self.score = 0
         self.update_score()
         
-        """Partie Alien"""
+        # Réinitialisation des aliens
         for alien in self.aliens:
             self.Canvas.delete(alien.id)
-        self.aliens = []  # Réinitialiser la liste
+        self.aliens = []  
 
+        # Exemple simple d'initialisation (à adapter à votre classe Alien)
         for i in range(5):  # 5 colonnes
             for j in range(3):  # 3 rangées
                 x = 100 + i * 50
@@ -88,30 +83,42 @@ class Jeu:
 
         self.deplacer_aliens()
 
-        """Partie Joueur"""
-        self.joueur = Player(self.Canvas, self.LARGEUR, self.HAUTEUR, interface)
+        # Initialisation joueur (à adapter à votre classe Player)
+        self.joueur = Player(self.Canvas, self.LARGEUR, self.HAUTEUR, self.interface)
 
     def update_score(self):
         """Met à jour l'affichage du score."""
         self.score_label.config(text=f"Score: {self.score}")
 
     def deplacer_aliens(self):
-        """Déplace tous les aliens à chaque intervalle."""
-        # Déplacer tous les aliens
-        for alien in self.aliens:
-            alien.deplacer_aliens(self.aliens)
-
-        # Relancer le mouvement après un court délai
+        """Déplace tous les aliens."""
+        if not self.jeu_en_pause:  # Vérifie si le jeu est en pause
+            for alien in self.aliens:
+                alien.deplacer_aliens(self.aliens)
         self.interface.after(16, self.deplacer_aliens)
-    
+
+    def afficher_menu_pause(self, event=None):
+        """Affiche le menu pause."""
+        self.jeu_en_pause = True  # Met le jeu en pause
+        menu = tk.Toplevel(self.interface)
+        menu.title("Menu Pause")
+        menu.geometry("300x200")
+        menu.transient(self.interface)
+        menu.grab_set()
+
+        # Ajouter des boutons
+        tk.Button(menu, text="Reprendre le jeu", command=lambda: [menu.destroy(), self.reprendre_jeu()]).pack(pady=10)
+        tk.Button(menu, text="Quitter", command=self.interface.quit).pack(pady=10)
+
+    def reprendre_jeu(self):
+        """Reprend le jeu après la pause."""
+        self.jeu_en_pause = False
+
     def deplacer_torpilles(self):
-        for torpille in Torpille.torpilles:
-            torpille.deplacer()
-            
-        self.interface.after(50, self.deplacer_torpilles)  # Appelle cette méthode toutes les 50 ms
-
-
-    
+        if not self.jeu_en_pause:  # Vérifie si le jeu est en pause
+            for torpille in Torpille.torpilles:
+                torpille.deplacer()
+        self.interface.after(50, self.deplacer_torpilles)
 
 
 # Lancer l'interface
